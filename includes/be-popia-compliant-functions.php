dfaf645e7797d42d193c15c08285afb36ba9b4b4
Francois <MAIL@FRANCOISJOUBERT.ME>
2021-11-10 09:15:13 +0200

    Version as on 10-11-2021

---
+++ includes/be-popia-compliant-functions.php
@@ -0,0 +1,20 @@
+<?php
+// List all tables
+global $wpdb;
+$mytables=$wpdb->get_results("SHOW TABLES");
+foreach ($mytables as $mytable)
+{
+    foreach ($mytable as $t) 
+    {       
+        echo $t . "<br>";
+    }
+}
+
+
+
+
+$user_id = 1;
+$processing_consent = null;
+add_user_meta( $user_id, '_processing_consent', $processing_consent);
+
+
